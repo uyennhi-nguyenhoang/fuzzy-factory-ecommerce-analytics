80c5248459e2fe778d54750f76e659b435918c59
Nhi Nguyen <119744093+uyennhi-nguyenhoang@users.noreply.github.com>
2026-05-04 02:34:28 +0700

    Update 03_revenue_summary_by_order.sql

--- sql/03_revenue_summary_by_order.sql
+++ sql/03_revenue_summary_by_order.sql
@@ -5,3 +5,10 @@
 -- and revenue used for orders KPI in the
 -- Sales Summary dashboard
 -- ============================================
+SELECT
+  DATE(created_at) AS date,
+  COUNT(DISTINCT order_id) AS orders,
+  SUM(price_usd) AS revenue
+FROM `e-commerce-project-489316.fuzzy_factory.orders`
+GROUP BY 1
+ORDER BY 1;

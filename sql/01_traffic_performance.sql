5b9daa1a288c975a20848c06e7f66d22990efea5
Nhi Nguyen <119744093+uyennhi-nguyenhoang@users.noreply.github.com>
2026-05-04 02:39:46 +0700

    Update 01_traffic_performance.sql
    simplify table name

--- sql/01_traffic_performance.sql
+++ sql/01_traffic_performance.sql
@@ -13,8 +13,8 @@ SELECT
   COUNT(DISTINCT ws.website_session_id) AS sessions,
   COUNT(DISTINCT o.order_id) AS orders,
   SUM(o.price_usd) AS revenue
-FROM `e-commerce-project-489316.fuzzy_factory.website_sessions` AS ws
-LEFT JOIN `e-commerce-project-489316.fuzzy_factory.orders` AS o
+FROM website_sessions AS ws
+LEFT JOIN orders AS o
   ON ws.website_session_id = o.website_session_id
 GROUP BY 1,2,3,4
 ORDER BY 1;

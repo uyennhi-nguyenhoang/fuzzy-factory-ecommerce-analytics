f47d707321ab7f1e04dcfe1126ff6beee5946fff
Nhi Nguyen <119744093+uyennhi-nguyenhoang@users.noreply.github.com>
2026-05-04 02:43:14 +0700

    Update 06_customer_type_by_month.sql

--- sql/06_customer_type_by_month.sql
+++ sql/06_customer_type_by_month.sql
@@ -5,3 +5,42 @@
 -- by new vs returning customers based on
 -- lifetime order history using ROW_NUMBER()
 -- ============================================
+WITH customer_orders AS 
+  (
+    SELECT
+        user_id,
+        order_id,
+        created_at,
+        DATE_TRUNC(created_at, MONTH) AS order_month,
+        ROW_NUMBER() OVER (
+            PARTITION BY user_id
+            ORDER BY created_at
+        ) AS order_number
+    FROM orders
+),
+
+
+classified_orders AS 
+  (
+    SELECT
+        *,
+        CASE
+            WHEN order_number = 1 THEN 'New'
+            ELSE 'Returning'
+        END AS customer_type
+    FROM customer_orders
+)
+
+
+SELECT
+    order_month,
+    COUNT(*) AS total_orders,
+    SUM(CASE WHEN customer_type = 'New'
+        THEN 1 ELSE 0 END) AS new_orders,
+    SUM(CASE WHEN customer_type = 'Returning'
+        THEN 1 ELSE 0 END) AS returning_orders,
+    ROUND(SUM(CASE WHEN customer_type = 'Returning'
+        THEN 1 ELSE 0 END) / COUNT(*), 4) AS returning_rate_pct
+FROM classified_orders
+GROUP BY order_month
+ORDER BY order_month;

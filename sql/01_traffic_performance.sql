30cedc07109033da6929800085d0a601441689b4
Nhi Nguyen <119744093+uyennhi-nguyenhoang@users.noreply.github.com>
2026-05-04 02:33:04 +0700

    Update 01_traffic_performance.sql

--- sql/01_traffic_performance.sql
+++ sql/01_traffic_performance.sql
@@ -1,1 +1,21 @@
+-- ============================================
+-- Fuzzy Factory | Maven Analytics
+-- Table: traffic_performance
+-- Description: Sessions, orders, and revenue
+-- aggregated by source, campaign, device,
+-- and date for digital performance analysis
+-- ============================================
+SELECT
+  DATE(ws.created_at) AS date,
+  ws.utm_source,
+  ws.utm_campaign,
+  ws.device_type,
+  COUNT(DISTINCT ws.website_session_id) AS sessions,
+  COUNT(DISTINCT o.order_id) AS orders,
+  SUM(o.price_usd) AS revenue
+FROM `e-commerce-project-489316.fuzzy_factory.website_sessions` AS ws
+LEFT JOIN `e-commerce-project-489316.fuzzy_factory.orders` AS o
+  ON ws.website_session_id = o.website_session_id
+GROUP BY 1,2,3,4
+ORDER BY 1;
 
